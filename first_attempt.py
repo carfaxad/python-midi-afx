449c5691337664526511e40f4971d8afffaecfd9
carfaxad <carlos.ortiz.ah@gmail.com>
2020-05-01 02:20:48 -0500

    Cleaning first attempt

--- first_attempt.py
+++ first_attempt.py
@@ -1,23 +1,3 @@
-# %%
-import mido
-import time
-import rtmidi
-
-# %%
-midiout = rtmidi.MidiOut()
-port = mido.open_output(midiout.get_ports()[0])
-
-# %%
-for note in range(40, 80, 1):
-    port.send(mido.Message('note_on', note=note, time=100))
-    if note % 2 == 0:
-        port.send(mido.Message('note_on', note=note + 4, time=100))
-    if note % 3 == 0:
-        port.send(mido.Message('note_on', note=note+7, time=100))
-    time.sleep(0.2)
-    # port.send(mido.Message('note_off'))
-    # time.sleep(0.1)
-
 # # %%
 # from mido import Message, MidiFile, MidiTrack
 
@@ -37,10 +17,3 @@ for note in range(40, 80, 1):
 # track.append(Message('note_off', note=64, velocity=127, time=100))
 
 # mid.save('new_song.mid')
-
-# %%
-from scales import ScaleModes
-s = ScaleModes()
-s.create_mode('dorico', 'C')
-
-# %%
